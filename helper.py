49b548eed537b17ef4b476eadcedbccf7a59db87
Ellen Xu <ellen2imagine@gmail.com>
2025-03-10 22:29:06 -0700

    helper function headers

---
+++ helper.py
@@ -0,0 +1,18 @@
+"""
+During inference, we map from real -> sim -> real
+
+Helper functions:
+
+1. Image processing: image -> T (x,y,theta)
+2. Forward kinematics: 6 joint angles -> calculate (x,y) in sim
+3. Inverse kinematics: (x,y) -> 6 joint angles
+"""
+
+def process_image(img):
+  pass
+
+def forward_kinematics():
+  pass
+
+def inverse_kinematics():
+  pass
