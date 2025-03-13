4f529cef1e4a2671c525cdc2aea2e828e2cb5b36
Ellen Xu <ellen2imagine@gmail.com>
2025-03-13 05:09:25 -0700

    doesn't work with passing in arm state to sim. added T into loop

--- run_sim2real.py
+++ run_sim2real.py
@@ -88,82 +88,63 @@ from helper import *
 from run_pusht import load_policy_from_checkpoint, policy_step
 # optional: start the env for render
 import matplotlib.pyplot as plt
-from diffusion_policy.env.pusht.pusht_env import PushTEnv
+from diffusion_policy.env.pusht.pusht_env_live import PushTEnvLive
+env = PushTEnvLive(render_size=96, render_action=True)
+env.reset()
 
 dataset = get_dataset()
 obs = []
-# policy = load_policy_from_checkpoint("models/pusht/0400-test_mean_score=0.844.ckpt")
-policy = load_policy_from_checkpoint("models/pusht/0500-test_mean_score=0.883.ckpt")
+policy = load_policy_from_checkpoint("models/pusht/0400-test_mean_score=0.844.ckpt")
 
-# get initial observation from real
-observation = robot.capture_observation()
-state = observation["observation.state"]
-image = observation["observation.images.phone"]
-
-print("Initial state: ", state)
-state = state.cpu().numpy()
-x, y = forward_kinematics(dataset, state, use_model=False)
-print("Initial forward kinematics: ", x, y)
-
-image = image.cpu().numpy()
-x_T, y_T, theta_T = get_t_position_and_orientation(image)
-x_T = x_T / 25.6
-y_T = y_T / 30
-theta_T = theta_T * np.pi / 180 # deg -> rad
-print("Initial overlayed grid cartesian coords [0, 16]: ", x, y, x_T, y_T, theta_T)
-
-x_sim, y_sim, x_T_sim, y_T_sim, theta_T_sim = real2sim_transform(x, y, x_T, y_T, theta_T)
-print("Initial sim coords [0, 512]: ", x_sim, y_sim, x_T_sim, y_T_sim, theta_T_sim)
-
-# set up env
-from diffusion_policy.gym_util.multistep_wrapper import MultiStepWrapper
-# env = MultiStepWrapper(
-#     PushTEnv(render_size=96),#, render_mode=render_mode),
-#     n_obs_steps=2,
-#     n_action_steps=8,
-#     max_episode_steps=inference_time_s * fps
-# )
-env = PushTEnv(render_size=96)
-
-env.seed(42)
-obs = env.reset()
-initial_obs = (x_sim, y_sim, x_T_sim, y_T_sim, theta_T_sim)
-env.set_state(*initial_obs)
-# obs = np.array([initial_obs, initial_obs])
-
-np_obs = np.array([initial_obs, initial_obs])
 for ts in range(inference_time_s * fps):
     start_time = time.perf_counter()
 
-    action = policy_step(np_obs, policy, device)
-    # TODO: do batch of 8 actions
-    action = action[0]
-    obs, reward, done, info = env.step(action)
-    
-    np_obs = np.array([obs, obs])
-    # np_obs = np_obs[-2:]
-
-    # update the T location
+    # Read the follower state and access the frames from the cameras
     observation = robot.capture_observation()
+
+    state = observation["observation.state"].cpu().numpy()
     image = observation["observation.images.phone"].cpu().numpy()
+
+    # state -> x,y
+    print("state: ", state)
+    x, y = forward_kinematics(dataset, state, use_model=False)
+    print("forward kinematics: ", x, y)
+
+    # image -> x_T, y_T, theta_T
+    # print("image", image.type())
     x_T, y_T, theta_T = get_t_position_and_orientation(image)
     x_T = x_T / 25.6
     y_T = y_T / 30
     theta_T = theta_T * np.pi / 180 # deg -> rad
-    
-    # convert T position
-    _, _, x_T_sim, y_T_sim, theta_T_sim = real2sim_transform(x, y, x_T, y_T, theta_T)
+    print("overlayed grid cartesian coords [0, 16]: ", x, y, x_T, y_T, theta_T)
+    # grid coords -> sim coords
+    x_sim, y_sim, x_T_sim, y_T_sim, theta_T_sim = real2sim_transform(x, y, x_T, y_T, theta_T)
+    # pixel -> grid
+    print("sim coords [0, 512]: ", x_sim, y_sim, x_T_sim, y_T_sim, theta_T_sim)
 
-    # x,y are still the same as in the obs
-    # update the T location
-    x, y = obs[0], obs[1]
-    env.set_state(x, y, x_T_sim, y_T_sim, theta_T_sim)
-    env.render(mode="human")
+    # render the state that was computed from image (where guy currently is)
+    env.set_state(x_sim, y_sim, x_T_sim, y_T_sim, theta_T_sim)
+    env.render("human")
 
-    #### SEND TO ROBOT ####
+    # model takes in observation context window
+    obs.append((x_sim, y_sim, x_T_sim, y_T_sim, theta_T_sim))
+    np_obs = np.array(obs)
+    if len(obs) < 2:
+        np_obs = np.array([obs[0], obs[0]])
+    else:
+        np_obs = np_obs[-2:]
+    assert np_obs.shape == (2,5), f"np_obs.shape: {np_obs.shape}"
+
+    sim_action = policy_step(np_obs, policy, device) # agent (x,y)
+    sim_action = sim_action[0] # TODO: check get last of action trajectory
+    print("sim pred: ", sim_action)
+
+    # render the action predicted by diffusion (where guy wants to be)
+    env.latest_action = sim_action
+    env.render("human")
 
     # sim coords -> grid coords    
-    action = sim2real_transform(*action)
+    action = sim2real_transform(*sim_action)
     print("overlayed grid action: ", action)
 
     # x,y -> action
@@ -176,3 +157,6 @@ for ts in range(inference_time_s * fps):
 
     dt_s = time.perf_counter() - start_time
     busy_wait(1 / fps - dt_s)
+
+total_time = time.perf_counter() - s_total
+print(f"Total time taken: {total_time} seconds")
